ba77d1f41da7ff99e996637953c6e2a7245affaf
ParkCY <42853965+Pkaato@users.noreply.github.com>
2022-11-06 20:45:48 +0900

    Initial Commit

---
+++ st/stApp.swift
@@ -0,0 +1,17 @@
+//
+//  stApp.swift
+//  st
+//
+//  Created by CHANGYONG PARK on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct stApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
